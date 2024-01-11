feddc25c497702428a0cd9398242784a88df64cc
Nolan Chen <43057150+nolanlc@users.noreply.github.com>
2024-01-11 08:50:54 -0800

    Add files via upload

---
+++ chat_with_documents.py
@@ -0,0 +1,124 @@
+import streamlit as st 
+from langchain.embeddings import BedrockEmbeddings
+from langchain.vectorstores import Chroma
+import os
+
+
+def load_document(file):
+    import os
+    
+    name, extension = os.path.splitext(file)
+    
+    if extension == '.pdf':
+        from langchain.document_loaders import PyPDFLoader
+        print(f'Loading {file}')
+        loader = PyPDFLoader(file)
+    elif extension == '.docx':
+        from langchain.document_loaders import Doc2txtLoader
+        print(f'Loading {file}')
+        loader = Docx2txtLoader(file)
+    elif extension == '.txt':
+        from langchain.document_loaders import TextLoader
+        loader = TextLoader(file)
+    else:
+        print('Document not supported')
+        return None
+    
+    data = loader.load() #returns 1 document per page
+    return data
+
+def chunk_data(data, chunk_size=256, chunk_overlap=20):
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    chunks = text_splitter.split_documents(data)
+    return chunks
+    
+def create_embeddings(chunks):
+    embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1")
+    print("calling Chroma.from_documents...")
+    vector_store = Chroma.from_documents(chunks, embeddings)
+    print("Success!")
+    return vector_store
+
+def ask_and_get_answer(vector_store, q, k=3):
+    from langchain.chains import RetrievalQA
+    from langchain.chat_models import BedrockChat
+
+    model_id='anthropic.claude-v2'
+    #model_id='amazon.titan-tg1-large'  #Titan doesn't work!
+
+
+    llm = BedrockChat(model_id=model_id, model_kwargs={"temperature": 1})
+
+    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k':k})
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+    
+    answer = chain.run(q)
+    return answer
+
+def calculate_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model("amazon.titan-embed-text-v1")
+    #total_tokens = sum([len(enc.encode(page.page_content)) f for page in texts])
+
+    total_tokens = 9999
+
+    return total_tokens, total_tokens/1000*0.0004
+
+
+if __name__ == '__main__':
+    import os
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv(), override=True)
+
+    #print("Hello Streamlit!")
+
+    st.image('img.jpg')
+    st.subheader('LLM Q/A App')
+    with st.sidebar:
+        api_key = st.text_input('API Key', type='password')
+        if api_key:
+            os.environ['BEDROCK_API_KEY'] = api_key
+
+        uploaded_file = st.file_uploader('Upload a file:', type=['pdf', 'docx','txt'])
+        chunk_size = st.number_input('Chunk size:', min_value=100, max_value=2048, value =512)
+        k = st.number_input('k',min_value=1, max_value=20, value=3)
+        add_data = st.button('Add Data')
+
+    if uploaded_file and add_data:
+        with st.spinner('Reading, chunking and embedding file...'):
+            bytes_data = uploaded_file.read()
+            file_name = os.path.join('./', uploaded_file.name)
+            with open(file_name, 'wb') as f:
+                f.write(bytes_data)
+
+            data = load_document(file_name)
+
+            chunks = chunk_data(data, chunk_size=chunk_size)
+            st.write(f'Chunk size: {chunk_size}, Chunks: {len(chunks)}')
+
+            st.write(f'Embedding Cost: Unknown')
+
+            vector_store = create_embeddings(chunks)
+        
+            st.session_state.vs = vector_store
+            st.success('File uploaded, chunked and embedded successfully')
+
+
+
+
+
+
+            
+                
+                
+
+
+
+
+                        
+
+
+
+    
+
