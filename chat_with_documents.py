24e833413b05893c7f94317295256ce0d3fc0e89
Nolan Chen <43057150+nolanlc@users.noreply.github.com>
2024-01-15 13:11:05 -0800

    Add files via upload

--- chat_with_documents.py
+++ chat_with_documents.py
@@ -131,7 +131,14 @@ if __name__ == '__main__':
             st.write(f'k: {k}')
             answer = ask_and_get_answer(vector_store,q,k)
             st.text_area('LLM Answer: ', value=answer)
-
+     
+            st.divider()
+            if 'history' not in st.session_state:
+                st.session_state.history =''
+            value = f'Q: {q} \nA: {answer}'
+            st.session_state.history = f'{value} \n {"-" * 100} \n {st.session_state.history}'
+            h = st.session_state.history
+            st.text_area(label='Chat History', value=h, key='history', height=400)
 
             
                 
